f694221dfcac8fdd7058c585630fed6d94429b6f
muyi-macpro <17621660286@163.com>
2018-09-20 00:11:58 +0800

    use re to split string

--- skills/handle_string/multi_split.py
+++ skills/handle_string/multi_split.py
@@ -5,7 +5,7 @@ __author__ = 'Jimu Yang'
 
 # 使用多种分割符分割字符串
 
-string = '1,34,64rw;we,tv\tyb;ti|314'
+string = '1,34,64rw,;we,tv\tyb;ti|314'
 
 res = string.split(',')
 print(res)
@@ -27,3 +27,20 @@ temp_list = []
 list(map(lambda x: temp_list.extend(x.split('|')), res))
 print(temp_list)
 res = temp_list
+
+def mysplit(string, ds):
+    res = [string]
+
+    for d in ds:
+        temp_list = []
+        list(map(lambda x: temp_list.extend(x.split(d)), res))
+        res = temp_list
+    return [x for x in res if x]
+
+print('-----')
+print(mysplit(string, ',;\t|'))
+
+# 使用re
+print('-----')
+import re
+print(re.split(r'[,;\t|]+', string))
